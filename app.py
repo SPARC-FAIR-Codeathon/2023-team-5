9f11bec5990adbc3e6ffab144e8255960d183b73
Alireza <you@example.com>
2023-08-07 20:05:02 +0200

    first demo

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import argparse
+import gradio as gr
 import os
 from langchain.chains import RetrievalQA
 from langchain.indexes import VectorstoreIndexCreator
@@ -8,13 +9,13 @@ from langchain.document_loaders import TextLoader
 from langchain.llms import HuggingFaceHub
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Query data from SPARC.")
-    parser.add_argument("hf_token", type=str, required=True,
+    parser.add_argument('--hf_token', type=str, required=True,
                         help="HuggingFace account token.")
-    parser.add_argument("txt_folder", type=str, default='./texts',
-                        help="Path to txt files.")
+    parser.add_argument("--txt_folder", type=str,
+                        default='./texts', help="Path to txt files.")
     args = parser.parse_args()
 
     txt_folder = args.txt_folder
@@ -40,3 +41,17 @@ if __name__ == 'main':
     qa = RetrievalQA.from_chain_type(llm=llm,
                                      chain_type="stuff",
                                      retriever=index.vectorstore.as_retriever())
+
+
+    def ask(query):
+        result = qa({"query": query})
+        return result["result"]
+
+
+    iface = gr.Interface(fn=ask,
+                         inputs=gr.components.Textbox(lines=7,
+                                                      label="Enter your text"),
+                         outputs="text",
+                         title="Ask your SPARC datasets anything!")
+
+    iface.launch(share=True)
